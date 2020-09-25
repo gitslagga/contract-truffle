b55a035fce9ee8eeeb38ce8ab77ff03907bd419d
slagga <957766762@qq.com>
2020-09-25 11:27:46 +0800

    add casino

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const GLDToken = artifacts.require("GLDToken");
 const GameItem = artifacts.require("GameItem");
+const Casino = artifacts.require("Casino")
 
 module.exports = function(deployer) {
     deployer.deploy(GLDToken, 1000);
     deployer.deploy(GameItem, "GameItem", "ITM");
+    deployer.deploy(Casino)
 };
