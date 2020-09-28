0a02fe4c3194ec118886ab54f127e8a0b8d134e7
slagga <957766762@qq.com>
2020-09-28 16:01:19 +0800

    add Defi contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,19 @@
 const GLDToken = artifacts.require("GLDToken");
 const GameItem = artifacts.require("GameItem");
 const Casino = artifacts.require("Casino")
+const Test = artifacts.require("Test");
+const TestPayable = artifacts.require("TestPayable");
+const Caller = artifacts.require("Caller")
+const DeFi = artifacts.require("DeFi");
 
 module.exports = function(deployer) {
     deployer.deploy(GLDToken, 1000);
     deployer.deploy(GameItem, "GameItem", "ITM");
     deployer.deploy(Casino)
+    
+    deployer.deploy(Test);
+    deployer.deploy(TestPayable);
+    deployer.deploy(Caller)
+    
+    deployer.deploy(DeFi);
 };
--- migrations/3_deploy_payable.js
+++
@@ -1,9 +0,0 @@
-const Test = artifacts.require("Test");
-const TestPayable = artifacts.require("TestPayable");
-const Caller = artifacts.require("Caller")
-
-module.exports = function(deployer) {
-    deployer.deploy(Test);
-    deployer.deploy(TestPayable);
-    deployer.deploy(Caller)
-};
