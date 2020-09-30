1f70405ecb11904cf7eabf95ee91fabc2e03f6a1
slagga <957766762@qq.com>
2020-09-30 11:45:50 +0800

    add normal deposit and withdraw

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0 <0.8.0;
 
-interface IERC20 {
+interface ITOKEN20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
 
     function balanceOf(address account) external view returns (uint256);
@@ -10,22 +10,46 @@ interface IERC20 {
 }
 
 contract MyContract {
-    IERC20 public usdt;
+    address payable public _owner;
+    ITOKEN20 public _token;
+    mapping (address => uint256) public _balances;
 
-    constructor(IERC20 _usdt) public {
-        usdt = _usdt;
+    constructor(ITOKEN20 token) public {
+        _owner = msg.sender;
+        _token = token;
+    }
+
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function changeOwner(address payable owner) external onlyOwner {
+        _owner = owner;
+    }    
+
+    //increase amount
+    function deposit() external payable returns (bool) {
+        _balances[msg.sender] += msg.value;
+        return true;
+    }
+
+    //decrease amount
+    function withdraw(uint256 amount) external onlyOwner returns (bool) {
+        _owner.transfer(amount);
+        return true;
     }
 
     //increase token's amount
-    //need user approve usdt token
-    function deposit(uint256 amount) external returns (bool) {
-        usdt.transferFrom(msg.sender, address(this), amount);
+    //need user approve token token
+    function depositToken(uint256 amount) external returns (bool) {
+        _token.transferFrom(msg.sender, address(this), amount);
         return true;
     }
 
     //decrease token's amount
-    function withdraw(uint256 amount) external returns (bool) {
-        usdt.transfer(msg.sender, amount);
+    function withdrawToken(uint256 amount) external returns (bool) {
+        _token.transfer(msg.sender, amount);
         return true;
     }
 }
--- test/MyContract.js
+++ test/MyContract.js
@@ -6,12 +6,29 @@ contract('MyContract', (accounts) => {
     let myContract;
     let gldToken;
     let contractAddress;
-    const amount = BigNumber(10).pow(18).multiply(100).toString(10);
+    const amount = BigNumber(10).pow(18).multiply(1).toString(10);
 
     before(async () => {
         gldToken = await GLDToken.new(web3.utils.toWei("1000"));
         myContract = await MyContract.new(gldToken.address);
         contractAddress = myContract.address;
+    });
+
+    it('creates deposit.', async () => {
+        await myContract.deposit({from:accounts[0], value:amount});
+
+        let _balances = await myContract._balances(accounts[0]);
+        assert.equal(_balances.valueOf(), amount);
+
+        let balance = await web3.eth.getBalance(contractAddress);
+        assert.equal(balance.valueOf(), amount);
+    });
+
+    it('creates withdraw.', async () => {
+        await myContract.withdraw(amount);
+
+        let balance = await gldToken.balanceOf(contractAddress);
+        assert.equal(balance.valueOf(), 0);
     });
 
     it('creates deposit usdt.', async () => {
@@ -20,7 +37,7 @@ contract('MyContract', (accounts) => {
         let allowance = await gldToken.allowance(accounts[0], contractAddress)
         assert.equal(allowance.valueOf(), amount);
 
-        await myContract.deposit(amount);
+        await myContract.depositToken(amount);
 
         allowance = await gldToken.allowance(accounts[0], contractAddress)
         assert.equal(allowance.valueOf(), 0);
@@ -30,7 +47,7 @@ contract('MyContract', (accounts) => {
     });
 
     it('creates withdraw usdt.', async () => {
-        await myContract.withdraw(amount);
+        await myContract.withdrawToken(amount);
 
         let balance = await gldToken.balanceOf(contractAddress);
         assert.equal(balance.valueOf(), 0);
