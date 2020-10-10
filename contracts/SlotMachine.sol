034df3bac4295e29627a3c5eaeca17f36696e33f
slagga <957766762@qq.com>
2020-10-10 15:06:19 +0800

    fix slot machine

--- contracts/SlotMachine.sol
+++ contracts/SlotMachine.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >0.6.0 <0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+pragma solidity >0.5.10 <0.8.0;
 
 contract SlotMachine {
     address payable private _owner;
     uint256 private _price;
     
-    mapping(string => uint256) private _multiple;
+    mapping(bytes32 => uint256) private _multiple;
     
     // event SlotPlay(string id, address player, );
     
-    constructor() public {
+    constructor() {
         _owner = msg.sender;
         _price = 1;
-        
+
         // apple, orange, mango, bell, watermelon, star, seven, bar
         // 2, 4, 5, 8, 10, 15, 20, 25
-        _multiple["apple"] = 2;
-        _multiple["orange"] = 4;
-        _multiple["mango"] = 5;
-        _multiple["bell"] = 8;
-        _multiple["watermelon"] = 10;
-        _multiple["star"] = 15;
-        _multiple["seven"] = 20;
-        _multiple["bar"] = 25;
+        _multiple[0x6170706c65000000000000000000000000000000000000000000000000000000] = 2;  //apple
+        _multiple[0x6f72616e67650000000000000000000000000000000000000000000000000000] = 4;  //orange
+        _multiple[0x6d616e676f000000000000000000000000000000000000000000000000000000] = 5;  //mango
+        _multiple[0x62656c6c00000000000000000000000000000000000000000000000000000000] = 8;  //bell
+        _multiple[0x77617465726d656c6f6e00000000000000000000000000000000000000000000] = 10; //watermelon
+        _multiple[0x7374617200000000000000000000000000000000000000000000000000000000] = 15; //star
+        _multiple[0x736576656e000000000000000000000000000000000000000000000000000000] = 20; //seven
+        _multiple[0x6261720000000000000000000000000000000000000000000000000000000000] = 25; //bar
     }
     
     modifier onlyOwner() {
@@ -40,8 +38,8 @@ contract SlotMachine {
         return _price;
     }
     
-    function getMultiple(string memory goal) public view returns (uint256) {
-        return _multiple[goal];
+    function getMultiple(bytes32 source) public view returns (uint256) {
+        return _multiple[source];
     }
 
     function changeOwner(address payable owner) public onlyOwner returns (bool) {
@@ -54,19 +52,32 @@ contract SlotMachine {
         return true;
     }
 
-    function play(bytes32[] memory goal, bytes32[] memory amount) public returns (bool) {
-        require(goal.length == amount.length, "goal and amount length do not match.");
+    function play(bytes32[] memory source, uint256[] memory amount) public returns (bool) {
+        require(source.length == amount.length, "source and amount length do not match.");
         
         //TODO play game
     }
     
-    function testByte(bytes32 gola) public pure returns (string memory) {
-        return string(abi.encodePacked(gola));
+    function convertBytes32ToString(bytes32 source) public pure returns (string memory) {
+        bytes memory tempSource = new bytes(32);
+        
+        for (uint i=0; i<32; i++) {
+            tempSource[i] = source[i];
+        }
+
+        return string(tempSource);
     }
     
     
-    function testString(string memory gola) public pure returns (bytes32) {
-        
-        return bytes32(gola);
+    function convertStringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempSource = bytes(source);
+
+        if (tempSource.length == 0) {
+            return 0x0;
+        }
+    
+        assembly {
+            result := mload(add(source, 32))
+        }
     }
 }
