ea998743b83c07876631261ab009d8ed243fc397
slagga <957766762@qq.com>
2020-10-10 16:43:05 +0800

    complete slot play game

--- contracts/SlotMachine.sol
+++ contracts/SlotMachine.sol
@@ -6,10 +6,13 @@ contract SlotMachine {
     uint256 private _price;
     
     mapping(bytes32 => uint256) private _multiple;
+    bytes32[] private _betting;
     
-    // event SlotPlay(string id, address player, );
     
-    constructor() {
+    event SlotPlay(address player, uint256 total);
+    event BetGoal(address player, uint256 num);
+    
+    constructor() public {
         _owner = msg.sender;
         _price = 1;
 
@@ -23,6 +26,15 @@ contract SlotMachine {
         _multiple[0x7374617200000000000000000000000000000000000000000000000000000000] = 15; //star
         _multiple[0x736576656e000000000000000000000000000000000000000000000000000000] = 20; //seven
         _multiple[0x6261720000000000000000000000000000000000000000000000000000000000] = 25; //bar
+        
+        _betting.push(0x6170706c65000000000000000000000000000000000000000000000000000000);
+        _betting.push(0x6f72616e67650000000000000000000000000000000000000000000000000000);
+        _betting.push(0x6d616e676f000000000000000000000000000000000000000000000000000000);
+        _betting.push(0x62656c6c00000000000000000000000000000000000000000000000000000000);
+        _betting.push(0x77617465726d656c6f6e00000000000000000000000000000000000000000000);
+        _betting.push(0x7374617200000000000000000000000000000000000000000000000000000000);
+        _betting.push(0x736576656e000000000000000000000000000000000000000000000000000000);
+        _betting.push(0x6261720000000000000000000000000000000000000000000000000000000000);
     }
     
     modifier onlyOwner() {
@@ -55,7 +67,36 @@ contract SlotMachine {
     function play(bytes32[] memory source, uint256[] memory amount) public payable returns (bool) {
         require(source.length == amount.length, "source and amount length do not match.");
         
-        //TODO play game
+        uint256 total = 0;
+        for (uint i = 0; i < amount.length; i++) {
+            total += _multiple[source[i]] * amount[i] * _price;
+        }
+
+        require(msg.value >= total, "msg value must get total amount.");
+
+        emit SlotPlay(msg.sender, total);
+
+        bytes32 goal = _betting[rand(8)];
+        for (uint i = 0; i < source.length; i++) {
+            if (source[i] == goal) {
+                uint256 num = _multiple[goal] * amount[i] * _price;
+                msg.sender.transfer(num);
+                emit BetGoal(msg.sender, num);
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    function withdraw(uint256 amount) external onlyOwner returns (bool) {
+        _owner.transfer(amount);
+        return true;
+    }
+    
+    function rand(uint256 _length) public view returns(uint256) {
+        uint256 random = uint256(keccak256(abi.encodePacked(block.difficulty, now)));
+        return random%_length;
     }
     
     function convertBytes32ToString(bytes32 source) public pure returns (string memory) {
